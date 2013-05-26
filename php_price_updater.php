ef3e67a7c1be97ab78b0cc11832224523d7041c7
nivong <nvoorn@gmail.com>
2013-05-26 18:50:08 +0300

    Add commit

--- php_price_updater.php
+++ php_price_updater.php
@@ -10,6 +10,7 @@ require 'app/Mage.php'; Mage::app();
 	foreach ($products as $product) {
 		echo $product->getName() .'<br>';
 		echo "old: " . $product->getPrice() .'<br>';
+			# if you want something different, change the + 5 down here (for percentage use * 1.10 for 10%)
 			$newPrice = ($product->getPrice() + 5);
 			$product->setPrice($newPrice);
 			$product->save();
